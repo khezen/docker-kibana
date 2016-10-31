de08c3a3a1724929d4867d8677463447b62e61d3
Guillaume Simonneau <milu@localhost.localdomain>
2016-10-31 22:37:50 +0100

    removing cat

--- src/edit_config.sh
+++ src/edit_config.sh
@@ -5,5 +5,3 @@ sed -ri "s/\{kibana_pwd\}/$kibana_pwd/" /etc/kibana/kibana.yml
 # ELASTICSEARCH URL
 sed -ri "s/\{elasticsearch_host\}/$elasticsearch_host/" /etc/kibana/kibana.yml
 sed -ri "s/\{elasticsearch_port\}/$elasticsearch_port/" /etc/kibana/kibana.yml
-
-cat /etc/kibana/kibana.yml
