22716900246437bbb6d7d0f4d1788381fc30f098
Guillaume Simonneau <milu@localhost.localdomain>
2016-11-01 17:06:34 +0100

    escaping sperator

--- src/edit_config.sh
+++ src/edit_config.sh
@@ -3,4 +3,4 @@
 # KIBANA PWD
 sed -ri "s/elasticsearch.password:[^\r\n]*/elasticsearch.password: $kibana_pwd/" /etc/kibana/kibana.yml
 # ELASTICSEARCH URL
-sed -ri "s/elasticsearch.url:[^\r\n]*/elasticsearch.url: http://$elasticsearch_host:$elasticsearch_port/" /etc/kibana/kibana.yml
+sed -ri "s/elasticsearch.url:[^\r\n]*/elasticsearch.url: http:\/\/$elasticsearch_host:$elasticsearch_port/" /etc/kibana/kibana.yml
