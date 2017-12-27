26551f60d60c307920e7788a42d03aa7ab25e619
Bertrand Martel <bmartel.fr@gmail.com>
2017-12-27 13:54:19 +0100

    add ability to configure server base path

--- src/miscellaneous/edit_config.sh
+++ src/miscellaneous/edit_config.sh
@@ -8,3 +8,7 @@ sed -i "s;.*server\.host:.*;server\.host: ${KIBANA_HOST};" "$CONF"
 sed -ri "s|elasticsearch.password:[^\r\n]*|elasticsearch.password: $KIBANA_PWD|" "$CONF"
 sed -ri "s|elasticsearch.ssl.verify:[^\r\n]*|elasticsearch.ssl.verify: true|" "$CONF"
 sed -ri "s|elasticsearch.ssl.ca:[^\r\n]*|elasticsearch.ssl.ca: /etc/elasticsearch/searchguard/ssl/ca/root-ca.pem|" "$CONF"
+# BASE PATH
+if [ ! -z $SERVER_BASE_PATH ]; then
+    sed -ri "s|#server.basePath:[^\r\n]*|server.basePath: $SERVER_BASE_PATH|" "$CONF"
+fi
