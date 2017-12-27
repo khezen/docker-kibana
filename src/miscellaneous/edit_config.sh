d2cb8229149a1641980bb1965a0b5fe1c3557d39
Bertrand Martel <bmartel.fr@gmail.com>
2017-12-27 13:26:10 +0100

    add ability to configure elasticsearch protocol (http/https)

--- src/miscellaneous/edit_config.sh
+++ src/miscellaneous/edit_config.sh
@@ -2,7 +2,7 @@
 
 CONF="/opt/kibana-$KIBANA_VERSION-linux-x86_64/config/kibana.yml"
 
-sed -ri "s|elasticsearch.url:[^\r\n]*|elasticsearch.url: https://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT|" "$CONF"
+sed -ri "s|elasticsearch.url:[^\r\n]*|elasticsearch.url: $ELASTICSEARCH_PROTOCOL://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT|" "$CONF"
 sed -i "s;.*server\.host:.*;server\.host: ${KIBANA_HOST};" "$CONF"
 
 sed -ri "s|elasticsearch.password:[^\r\n]*|elasticsearch.password: $KIBANA_PWD|" "$CONF"
